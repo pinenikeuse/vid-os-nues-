b6ecc264e9d250a4cd4bf8a008350f9a305d6b95
pinenikeuse <baizemi0039@gmail.com>
2025-05-18 05:15:01 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,8 @@
-require('dotenv').config();
 const express = require('express');
-const axios = require('axios');
-
+const path = require('path');
 const app = express();
-app.use(express.json());
 
-// Sert les fichiers statiques du dossier "public"
-app.use(express.static('public'));
-
-const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
-const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
-
-async function sendTelegramMessage(message) {
-  const url = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
-  await axios.post(url, {
-    chat_id: TELEGRAM_CHAT_ID,
-    text: message,
-  });
-}
-
-app.post('/send-notification', async (req, res) => {
-  const { message } = req.body;
-  if (!message) return res.status(400).send('Message manquant.');
-
-  await sendTelegramMessage(message);
-  res.send('Notification envoyée sur Telegram.');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Serveur démarré sur le port ${PORT}`));
